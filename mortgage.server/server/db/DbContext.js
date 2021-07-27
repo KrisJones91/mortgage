935612d3d9d0e8ba18fc474e9ce93642a04f22c7
Kris Jones <kristop.jones@gmail.com>
2021-07-27 13:08:49 -0600

    trackers

--- mortgage.server/server/db/DbContext.js
+++ mortgage.server/server/db/DbContext.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 import { Value as ValueSchema } from '../models/Value'
 import { AccountSchema } from '../models/Account'
 import GoalsSchema from '../models/Goals'
-import TrackerSchema from '../models/Tracker'
+import TrackerSchema from '../models/Trackers'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
